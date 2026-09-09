88a004a36a932311bf3428c0205b104dde50525e
Alonso Bonansco <al.bonansco@duocuc.cl>
2026-09-09 19:13:01 -0300

    feat: completar lógica de Repartidor y conectar con Pedido y ZonaDeCarga

--- src/main/java/cl/duoc/speedfast/model/Repartidor.java
+++ src/main/java/cl/duoc/speedfast/model/Repartidor.java
@@ -2,28 +2,56 @@ package cl.duoc.speedfast.model;
 
 import cl.duoc.speedfast.service.ZonaDeCarga;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 // Implementa Runnable
-public class Repartidor {
-    private String nombreRepartidor;
-    private ZonaDeCarga zonaDeCarga;
+public class Repartidor implements Runnable {
+    private final String nombreRepartidor;
+    private final ZonaDeCarga zonaDeCarga;
 
     public Repartidor(String nombreRepartidor, ZonaDeCarga zonaDeCarga) {
         this.nombreRepartidor = nombreRepartidor;
         this.zonaDeCarga = zonaDeCarga;
     }
 
-    /*
-        getters o setters?
-     */
-
-    /*
     @Override
-    run →
-        retirar un pedido
-        cambiar estado a EN_REPARTO y mostrar un mensaje
-        simular entrega con Thread.sleep
-        cambiar estado a ENTREGADO y mostar un mensaje final
-     */
+    public void run() {
+        while (true) {
+            Pedido pedido = zonaDeCarga.retirarPedido();
 
+            if (pedido == null) {
+                break;
+            }
 
+            try {
+                int tiempoPausas = 1000 + ThreadLocalRandom.current().nextInt(2500);
+
+                TimeUnit.MILLISECONDS.sleep(tiempoPausas);
+
+                System.out.println("Retirando pedido #" + pedido.getIdPedido() + "... [Repartidor " +
+                        nombreRepartidor + "]");
+                TimeUnit.MILLISECONDS.sleep(tiempoPausas);
+
+                pedido.setEstadoPedido(EstadoPedido.EN_REPARTO);
+                System.out.println("Estado pedido #" + pedido.getIdPedido() +
+                        ": " + pedido.getEstadoPedido());
+
+                TimeUnit.MILLISECONDS.sleep(tiempoPausas);
+
+                System.out.println("Entregando pedido #" + pedido.getIdPedido() + "...");
+
+                TimeUnit.MILLISECONDS.sleep(tiempoPausas);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Entrega interrumpida");
+                break;
+            }
+
+            pedido.setEstadoPedido(EstadoPedido.ENTREGADO);
+            System.out.println("Estado pedido #" +
+                    pedido.getIdPedido() + ": " + pedido.getEstadoPedido() + " por [Repartidor " + nombreRepartidor
+                    + "]");
+        }
+    }
 }
--- src/main/java/cl/duoc/speedfast/service/ZonaDeCarga.java
+++ src/main/java/cl/duoc/speedfast/service/ZonaDeCarga.java
@@ -8,12 +8,13 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ZonaDeCarga {
     private final BlockingQueue<Pedido> pedidosPendientes = new LinkedBlockingQueue<>();
 
-    // El uso de 'synchronized' no es necesario
-    public synchronized void agregarPedido(Pedido pedido) {
+    // El uso de 'synchronized' no es necesario porque BlockingQueue es internamente segura para hilos
+    public void agregarPedido(Pedido pedido) {
         pedidosPendientes.add(pedido);
+        System.out.println("Pedido #" + pedido.getIdPedido() + " agregado. Destino: " + pedido.getDireccionEntrega());
     }
 
-    public synchronized Pedido retirarPedido() {
+    public Pedido retirarPedido() {
         return pedidosPendientes.poll();
     }
 }
